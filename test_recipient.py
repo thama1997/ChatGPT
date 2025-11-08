a7ea1e2c27412b2e6c11662b7b8a87654649eb3a
thama1997 <thama1997@yahoo.com>
2025-11-08 15:22:12 +0100

    initial

---
+++ test_recipient.py
@@ -0,0 +1,24 @@
+import asyncio
+import json
+
+from revChatGPT.V1 import AsyncChatbot
+from revChatGPT.V1 import Chatbot
+
+config = json.load(open("/home/acheong/.config/revChatGPT/config.json"))
+
+
+async def main() -> None:
+    chatbot = AsyncChatbot(config)
+    async for message in chatbot.ask("Hello, how are you?"):
+        print(message.get("message"))
+
+    print(await chatbot.share_conversation())
+
+
+def sync_main():
+    chatbot = Chatbot(config)
+    for message in chatbot.ask("Hello, how are you?"):
+        print(message.get("message"))
+
+
+asyncio.run(main())
